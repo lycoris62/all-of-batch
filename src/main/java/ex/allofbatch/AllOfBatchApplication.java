40e22dbee6b13450dd4627242608117ac9a817f2
yunjae62 <wodbs7758@naver.com>
2024-10-07 23:38:53 +0900

    first commit

---
+++ src/main/java/ex/allofbatch/AllOfBatchApplication.java
@@ -0,0 +1,12 @@
+package ex.allofbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllOfBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllOfBatchApplication.class, args);
+    }
+}
---
+++ src/test/java/ex/allofbatch/AllOfBatchApplicationTests.java
@@ -0,0 +1,12 @@
+package ex.allofbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllOfBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
